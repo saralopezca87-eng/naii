7d7d3168509b956b6bca3fb199e0b6cb56326dd5
Robinson lopez <robinsonlopez1199@gmail.com>
2026-01-10 01:39:42 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# CONFIGURACIÓN GENERAL
+
+TOTAL_VALVES = 12
+
+# GPIO pins (ajústalos luego)
+VALVE_PINS = {
+    1: 17,
+    2: 18,
+    3: 27,
+    4: 22,
+    5: 23,
+    6: 24,
+    7: 25,
+    8: 5,
+    9: 6,
+    10: 12,
+    11: 13,
+    12: 19,
+}
+
+RELAY_ACTIVE_STATE = 1  # HIGH
